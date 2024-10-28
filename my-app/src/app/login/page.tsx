fbba9d00d678c1848e4ac60a96e8368c9517fbc3
Victor Zion França <vzion435@gmail.com>
2024-10-28 14:31:58 -0300

    alterações

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { FaGithub, FaEnvelope } from "react-icons/fa"; // Importa os ícones do GitHub e de e-mail
+import { MdVisibility, MdVisibilityOff } from "react-icons/md"; // Ícones para mostrar e esconder a senha
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Estado para controlar a visibilidade da senha
   const router = useRouter(); // Use o hook useRouter para redirecionar
 
   const handleSubmit = async (event: React.FormEvent) => {
@@ -40,13 +42,22 @@ export default function LoginPage() {
             placeholder="Email"
             className="p-2 border rounded"
           />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Senha"
-            className="p-2 border rounded"
-          />
+          <div className="relative"> {/* Div para posicionar o ícone de mostrar/esconder senha */}
+            <input
+              type={showPassword ? "text" : "password"} // Altera o tipo com base no estado
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Senha"
+              className="p-2 border rounded w-full"
+            />
+            <div
+              onClick={() => setShowPassword(!showPassword)} // Alterna a visibilidade da senha
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-black" // Estilização do ícone
+              aria-label={showPassword ? "Esconder senha" : "Mostrar senha"} // Acessibilidade
+            >
+              {showPassword ? <MdVisibility /> : <MdVisibilityOff />} {/* Ícone de mostrar/esconder senha */}
+            </div>
+          </div>
 
           {error && <p className="text-red-500 mt-2 text-xs">{error}</p>}
 
